f254763c6ed9dfe59fd386a554778ac7db65f63f
christianvoigt <christianvoigt@users.noreply.github.com>
2021-09-10 11:21:43 +0000

    deploy: c4afbdffc3b166273d33e242fb08d136e457862f

--- inferenceline.lua
+++
@@ -1,5 +0,0 @@
-local inferencerule = [[\vspace{-2\parskip}\hspace{14pt}\rule{0.3\linewidth}{0.5pt}\vspace{-\parskip}]]
-
-function HorizontalRule(el)
-    return pandoc.RawBlock('tex', inferencerule)
-end
