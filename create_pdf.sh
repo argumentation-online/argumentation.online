cb05124ec7e8c2984c9e23bdaea907e84517cc46
Christian Voigt <1pxsolidblack@gmail.com>
2021-09-10 15:13:06 +0200

    removed pdf folder from repo, moved pdf style files into pdf-styles

--- create_pdf.sh
+++ create_pdf.sh
@@ -4,7 +4,7 @@ ID="AUTOR1-AUTOR2_ArgOnl-JAHR-NUMMER"
 
 pandoc posts/$ID.md \
   -o pdfs/$ID.pdf \
-  --metadata-file=pdfs/styles/meta_aao-latex.yaml \
-  --template=pdfs/styles/argonl_latex.template \
+  --metadata-file=pdf-styles/meta_aao-latex.yaml \
+  --template=pdf-styles/argonl_latex.template \
   --pdf-engine=xelatex \
-  --lua-filter=inferenceline.lua
+  --lua-filter=pdf-styles/inferenceline.lua
